35a343cb52c4af6ef918e7f8f3dc12f12a5039fd
Fanlushuai@outlook.com <12054311587ok>
2019-08-07 04:22:22 +0800

    修改图片正则规则

--- downloadcnbingpic.py
+++ downloadcnbingpic.py
@@ -1,11 +1,11 @@
-import re
-import urllib.request
 import os
+import re
 import time
+import urllib.request
 
 
 def get_html(url):
-    #print("开始获取页面源代码...")
+    # print("开始获取页面源代码...")
 
     # 隐藏python
     request = urllib.request.Request(url)
@@ -16,21 +16,22 @@ def get_html(url):
     response = urllib.request.urlopen(request)
     html = response.read().decode("utf-8")
 
-    #print("源代码获取成功...")
+    # print("源代码获取成功...")
     return html
 
 
 def get_imglist(html):
-    #print("开始获取图片地址...")
+    # print("开始获取图片地址...")
 
-    r_img = r'((?:http)[^,]*cn\.bing\.net[^,]*1080[^,]*.(?:jpg|png))'  # 正则表达式
+    # r_img = r'((?:http)[^,]*cn\.bing\.net[^,]*1080[^,]*.(?:jpg|png))'  # 正则表达式
+    r_img = r'(url\()([^(]*?1920x1080[^)]*?)(\))'  # 正则表达式
     p_img = re.compile(r_img)  # 正则编译
 
-    imglist = p_img.findall(html)
-    #for img in imglist:
-        #print(img)
-    #print("图片地址获取成功")
-    return imglist
+    imglist = p_img.search(html).group(2)
+    # for img in imglist:
+    # print(img)
+    # print("图片地址获取成功")
+    return 'http://cn.bing.com' + imglist
 
 
 def save_pic(imglist):
@@ -39,24 +40,31 @@ def save_pic(imglist):
     bak_path = script_path + "\\cnBingImage_bak\\"
     name = path + "1.jpg"
     if not os.path.exists(path):
-        #print("创建目录..." + path)
+        # print("创建目录..." + path)
         os.makedirs(path)
-    #print("开始下载图片...")
-    for img in imglist:
-        conn = urllib.request.urlopen(img)
+    # print("开始下载图片...")
+    if isinstance(imglist, str):
+        conn = urllib.request.urlopen(imglist)
         f = open(name, 'wb')
         f.write(conn.read())
         f.flush()
         f.close()
+    else:
+        for img in imglist:
+            conn = urllib.request.urlopen(img)
+            f = open(name, 'wb')
+            f.write(conn.read())
+            f.flush()
+            f.close()
     copy_file(name, bak_path)
-    #print("图片下载完成...")
+    # print("图片下载完成...")
 
 
 def copy_file(name, todir):
-    #print("备份文件开始...")
+    # print("备份文件开始...")
     old_file = open(name, 'rb')
     if not os.path.exists(todir):
-        #print("创建目录..." + todir)
+        # print("创建目录..." + todir)
         os.makedirs(todir)
     new_file_name = today_date_str() + os.path.splitext(name)[1]
     new_file = open(todir + new_file_name, 'wb')
@@ -64,7 +72,7 @@ def copy_file(name, todir):
     old_file.flush()
     old_file.close()
     new_file.close()
-    #print("备份文件结束..." + new_file_name)
+    # print("备份文件结束..." + new_file_name)
 
 
 def today_date_str():
